8f022a6b8da5d873b4d2fd836ba11f88286ff323
marikf98 <95852164+marikf98@users.noreply.github.com>
2023-05-01 10:36:33 +0300

    Add files via upload

---
+++ ImageSearch/ImageSearch/MTMergeSort.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Threading;
+using System.Threading.Tasks;
+namespace ImageSearch
+{
+    public class MTMergeSort
+    {
+        /*public List <string> MergeSort(string[] starList, int nMin = 2)
+        {
+            Sort(starList);
+            return null;
+        }*/
+        public List<(string t)> MergeSort(string[] starList, int nMin = 2)
+        {
+            
+        }
+
+        public static void Sort(string[] startList)
+        {
+            /*if (startList.Length > 1)
+            {
+                int mid = startList.Length / 2;
+
+                string[] firstHalf = new string[mid];
+                string[] secondHalf = new string[startList.Length - mid];
+                Array.Copy(startList,0, firstHalf, 0,mid);
+                Array.Copy(startList, mid, secondHalf, 0, startList.Length - mid);
+
+                Thread threadFirstHalf = new Thread(() => Sort(firstHalf));
+                Thread threadSecondHalf = new Thread(() => Sort(secondHalf));
+
+                threadFirstHalf.Start();
+                threadSecondHalf.Start();
+
+                threadFirstHalf.Join();
+                threadSecondHalf.Join();
+
+                Merge(firstHalf, secondHalf, startList);
+            }*/
+            
+            if (startList.Length > 1)
+            {
+                string[] arrays = new string[]
+            }
+        }
+        
+        private static void Merge(string[] first, string[] second, string[] arr)
+        {
+            int currFirst = 0;
+            int currSecond = 0;
+            int currArr = 0;
+
+            while(currFirst < first.Length && currSecond < second.Length)
+            {
+                if (string.Compare(first[currFirst], second[currSecond]) == -1) // second is bigger
+                    {
+                        arr[currArr] = first[currFirst];
+                        currFirst++;
+                        currArr++;
+                    }
+                    else
+                    {
+                        arr[currArr] = second[currSecond];
+                        currSecond++;
+                        currArr++;
+                    }
+                
+                
+                
+                    /*if (string.Compare(first[currFirst], second[currSecond]) == 1) // first is bigger
+                    {
+                        arr[currArr] = second[currSecond];
+                        currSecond++;
+                        currArr++;
+                    }
+                    
+                    else
+                    {
+                        arr[currArr] = first[currFirst];
+                        currFirst++;
+                        currArr++;
+                        arr[currArr] = second[currSecond];
+                        currSecond++;
+                        currArr++;
+                    }*/
+            }
+
+            while (currFirst < first.Length)
+            {
+                arr[currArr] = first[currFirst];
+                currFirst++;
+                currArr++;
+            }
+            while (currSecond < second.Length)
+            {
+                arr[currArr] = second[currSecond];
+                currSecond++;
+                currArr++;
+            }
+        }
+    }
+
+
+
+
+
+    /*class MyTask
+    {
+        private string[] starList;
+        
+        public MyTask(string[] arr)
+        {
+            this.starList = arr;
+        }
+
+        public void Compute()
+        {
+            if (starList.Length > 1)
+            {
+                int mid = starList.Length / 2;
+
+                string[] firstHalf = new string[mid];
+                string[] secondHalf = new string[starList.Length - mid];
+                Array.Copy(starList,firstHalf,mid);
+                Array.Copy(starList, mid, secondHalf, 0, starList.Length - mid);
+
+                Task firstTask = Task.Factory.StartNew(() => new MyTask(firstHalf).Compute());
+                Task secondTask = Task.Factory.StartNew(() => new MyTask(secondHalf).Compute());
+                Task.WaitAll(firstTask, secondTask);
+
+                MTMergeSort.Merge(firstHalf, secondHalf,starList);
+
+            }
+        }
+    }*/
+    
+
+
+
+}
---
+++ ImageSearch/ImageSearch/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ImageSearch
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int counter = 0;
+            string[] arr1 = { "Dog", "Cat", "elephant", "ant", "Bird" };
+            MTMergeSort mergeSort = new MTMergeSort();
+            mergeSort.MergeSort(arr1);
+            string[] arr1Test = { "ant", "Bird", "Cat", "Dog", "elephant" };
+            if (arr1 == arr1Test)
+            {
+                counter++;
+            }
+            string[] arr2 = {"Dog", "Cat", "Dog", "Elephant", "Cat", "Elephant" , "Bird"};
+            MTMergeSort mergeSort2 = new MTMergeSort();
+            mergeSort.MergeSort(arr2);
+            string[] arr2Test = { "Bird", "Cat", "Dog", "Dog", "Elephant", "Elephant" };
+            if (arr2 == arr2Test)
+            {
+                counter++;
+            }
+            
+            
+
+        }
+    }
+}
