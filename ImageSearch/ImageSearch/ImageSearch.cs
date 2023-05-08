4698298fc9852c87e5fd55d50b4a754fbb6f28a5
marikf98 <marikf98@gmail.com>
2023-05-08 17:41:47 +0300

    Final

--- ImageSearch/ImageSearch/ImageSearch.cs
+++ ImageSearch/ImageSearch/ImageSearch.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -11,36 +12,69 @@ using System.Threading;
 
 namespace ImageSearch
 {
+    /*
+     *This is the image search class that can search for a small picture in a big picture
+     * The algorithm splits the big pictures into tiles that are a bit bigger then the small picture and the tiles overlap each over
+     * so the image wont be missed on the stitch
+     */
     public class ImageSearch
     {
-        public static void FirstSearchOption(string[] args)
+        /*
+         *This is the main function of the class  
+         */
+        public static void SearchImage(string[] args)
         {
-            if (args.Length < 3)
+            if (args.Length < 3) // input check for the arguments 
             {
                 Console.WriteLine("Not enough arguments ");
                 Environment.Exit(1);
             }
+            
             string image1Input = args[0]; // the bigger picture
             string image2Input = args[1]; // the smaller picture
             int nThreadsInput = int.Parse(args[2]);
             string algorithmInput = args[3];
-            
-            Image image1 = Image.FromFile(image1Input);
-            Image image2 = Image.FromFile(image2Input);
-            Bitmap bitMapImage2 = new Bitmap(image2Input);
+            // input check for the file existence 
+            if (!File.Exists(image2Input))
+            {
+                Console.WriteLine("Image file doesnt exists.");
+                Environment.Exit(1);
 
+            }
+            if (!File.Exists(image1Input))
+            {
+                Console.WriteLine("Image file doesnt doesnt exists.");
+                Environment.Exit(1);
+
+            }
+            
+            //We convert the the string input of the image source to an image object 
+            Image image1 = null;
+            Image image2 = null;
+            // here we check their validity 
+            try
+            {
+                image1 = Image.FromFile(image1Input);
+                image2 = Image.FromFile(image2Input);
+                Bitmap bitMapImage2 = new Bitmap(image2Input);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The image files are not valid");
+                Environment.Exit(1);
+            }
+
+            // Here we calculate the size of each tile 
             int smallWidth = image2.Width;
             int smallHeight = image2.Height;
             int overlapWidth = smallWidth / 2;
             int overlapHeight = smallHeight / 2;
             
-            // int numTilesX = (image1.Width - overlapWidth) / (smallWidth - overlapWidth);
-            // int numTilesY = (image1.Height - overlapHeight) / (smallHeight - overlapHeight);
-            
+            // and here we calculate how many tiles will be on each axis 
             int numTilesX = (int)Math.Ceiling((double)(image1.Width - overlapWidth) / (smallWidth - overlapWidth));
             int numTilesY = (int)Math.Ceiling((double)(image1.Height - overlapHeight) / (smallHeight - overlapHeight));
             
-
+            //Here we create a matrix of bitmaps and points according to the tiles that we calculated before
             Bitmap[,] tiles = new Bitmap[numTilesX,numTilesY];
             Point[,] pointsOffset = new Point[numTilesX, numTilesY];
 
@@ -48,11 +82,12 @@ namespace ImageSearch
             {
                 for (int j = 0; j < numTilesY; j++)
                 {
+                    // here we calculate the start coordinates of each tile
                     int startX = i * (smallWidth - overlapWidth);
                     int startY = j * (smallHeight - overlapHeight);
                     int searchWidth = smallWidth + overlapWidth;
                     int searchHeight = smallHeight + overlapHeight;
-
+                    // here we crate the bitmap and the point
                     Bitmap tile = new Bitmap(searchWidth, searchHeight);
 
                     using (Graphics g = Graphics.FromImage(tile))
@@ -64,8 +99,10 @@ namespace ImageSearch
                     tiles[i, j] = tile;
                 }
             }
+            // after we created the matrix we convert them to single dimensional array for ease wit the thread splitting
             Bitmap[] oneDtiles = tiles.Cast<Bitmap>().ToArray();
             Point[] point1D = pointsOffset.Cast<Point>().ToArray();
+            // here we create a helper array of the smaller image as a bit map so each thread can read from his own bitmap and there wont be collisions 
             Bitmap[] compareBitmap = new Bitmap[nThreadsInput];
             
             for (int i = 0; i < nThreadsInput; i++)
@@ -73,15 +110,17 @@ namespace ImageSearch
                 compareBitmap[i] = new Bitmap(image2Input);
             }
             
-            int tilesPerThread = (int)Math.Ceiling((double)(numTilesX * numTilesY) / nThreadsInput); // make sure that all the tiles are scanned
+            int tilesPerThread = (int)Math.Ceiling((double)(numTilesX * numTilesY) / nThreadsInput); // the number of tiles each tread will scan at most 
 
             List<Point?[]> resultFromExact = new List<Point?[]>();
             List<Thread> threads = new List<Thread>();
+            // we created a 2d array so each thread will scan its own array of point and bitmap 
             Bitmap[][] bitmapSubArrays = new Bitmap[nThreadsInput][];
             Point[][] pointSunArrays = new Point[nThreadsInput][];
             
             int subArraySize = (int)Math.Ceiling((double)oneDtiles.Length / nThreadsInput);
             
+            // and here we fill each sub array with the tiles that each thread will scan
             for (int i = 0; i < nThreadsInput; i++)
             {
                 int startIndex = i * subArraySize;
@@ -93,59 +132,71 @@ namespace ImageSearch
                 int subArrayLength = endIndex - startIndex;
                 if (subArrayLength < 0)
                 {
-                    subArrayLength = subArrayLength * (-1);}
+                    subArrayLength = subArrayLength * (-1);
+                }
                 bitmapSubArrays[i] = new Bitmap[subArrayLength];
                 pointSunArrays[i] = new Point[subArrayLength];
                 Array.Copy(oneDtiles, startIndex, bitmapSubArrays[i], 0, subArrayLength);
                 Array.Copy(point1D, startIndex, pointSunArrays[i], 0, subArrayLength);
             }
 
-
-            if (algorithmInput == "exact")
+            /*
+             * here we create the threads for the algorithm that the user wants
+             * we add them to the thread array 
+             */
+            switch (algorithmInput)
             {
-                for (int i = 0; i < nThreadsInput; ++i)
+                case "exact":
                 {
-                    int index = i;
-                    Thread thread = new Thread(() =>
-                        {
-                            resultFromExact.Add(FindBitMapExact(bitmapSubArrays[index], compareBitmap[index], pointSunArrays[index]));
-                        }
-                    );
-                    threads.Add(thread);
-                }
-            }
+                    for (int i = 0; i < nThreadsInput; ++i)
+                    {
+                        int index = i;
+                        Thread thread = new Thread(() =>
+                            {
+                                resultFromExact.Add(FindBitMapExact(bitmapSubArrays[index], compareBitmap[index], pointSunArrays[index]));
+                            }
+                        );
+                        threads.Add(thread);
+                    }
 
-            else if(algorithmInput == "euclidian")
-            {
-                for (int i = 0; i < nThreadsInput; ++i)
+                    break;
+                }
+                case "euclidian":
                 {
-                    int index = i;
-                    Thread thread = new Thread(() =>
-                        {
-                            resultFromExact.Add(FindBitMapEuclidian(bitmapSubArrays[index], compareBitmap[index], pointSunArrays[index]));
-                        }
-                    );
-                    threads.Add(thread);
+                    for (int i = 0; i < nThreadsInput; ++i)
+                    {
+                        int index = i;
+                        Thread thread = new Thread(() =>
+                            {
+                                resultFromExact.Add(FindBitMapEuclidian(bitmapSubArrays[index], compareBitmap[index], pointSunArrays[index]));
+                            }
+                        );
+                        threads.Add(thread);
+                    }
+
+                    break;
                 }
+                //if the algorithm input is invalid we print an error message and exit 
+                default:
+                    Console.WriteLine("Please enter exact or euclidian");
+                    Environment.Exit(1);
+                    break;
             }
-            else
-            {
-                Console.WriteLine("Please enter exact or euclidian");
-                Environment.Exit(1);
 
-            }
-
-            
+            //then we launch the treads 
             foreach (Thread thread in threads)
             {
                  thread.Start();
             }
-            
+            //and wait for the threads to finish
             foreach (Thread thread in threads)
             {
                 thread.Join();
             }
-            
+            /*
+             * here we collect all the results of each thread and create the final result array
+             * we scan each sub array and check if we got any point of the start location of the small photo
+             */
             List<Point?> resultsExact = new List<Point?>();
             foreach (Point?[] pointArr in resultFromExact)
             {
@@ -155,10 +206,7 @@ namespace ImageSearch
                 }
                 resultsExact.AddRange(pointArr);
             }
-            
-            
-            
-            //List<Point?> finalResultsExact = new List<Point?>();
+
             HashSet<Point?> finalResultsExact = new HashSet<Point?>();
 
             for (int i = 0; i < resultsExact.Count; i++)
@@ -168,54 +216,53 @@ namespace ImageSearch
                     finalResultsExact.Add(resultsExact[i]);
                 }
             }
-            
+            //Finally we print out all the locations that we found 
             foreach (Point point in finalResultsExact)
             {
-                Console.WriteLine(point.X + "," + point.Y);
+                Console.WriteLine(point.Y + "," + point.X);
             }
-            
         }
         
-        
+        /*
+         * This is the exact search function
+         */
         private static Point?[] FindBitMapExact(Bitmap[] bitmapsToSearch, Bitmap bitmapToFind, Point[] offset)
         {
-            if (bitmapsToSearch == null || bitmapToFind == null || offset == null)
+            if (bitmapsToSearch == null || bitmapToFind == null || offset == null) // validity check
             {
                 return null;
             }
            
             Point?[] results = new Point?[bitmapsToSearch.Length];
+            /*
+             *  Here we iterate over each pixel of the tile and compare them to the small image 
+             */
             for (int k = 0; k < bitmapsToSearch.Length; k++)
             {
-                //Console.WriteLine("bitmapsToSearch.Length = " + bitmapsToSearch.Length);
                 for (int x = 0; x < bitmapsToSearch[k].Width - bitmapToFind.Width + 1; x++)
                 {
-                    //Console.WriteLine("bitmapsToSearch[k].Width - bitmapToFind.Width + 1 = " + (bitmapsToSearch[k].Width - bitmapToFind.Width + 1));
-
                     for (int y = 0; y < bitmapsToSearch[k].Height - bitmapToFind.Height + 1; y++)
                     {
-                        //Console.WriteLine("bitmapsToSearch[k].Height - bitmapToFind.Height + 1 = " + (bitmapsToSearch[k].Height - bitmapToFind.Height + 1));
-
                         bool match = true;
 
                         for (int i = 0; i < bitmapToFind.Width; i++)
                         {
                             for (int j = 0; j < bitmapToFind.Height; j++)
                             {
-                                if (bitmapsToSearch[k].GetPixel(x + i, y + j) != bitmapToFind.GetPixel(i, j))
+                                if (bitmapsToSearch[k].GetPixel(x + i, y + j) != bitmapToFind.GetPixel(i, j)) // if there is different pixels then  the pictures dont match
                                 {
                                     match = false;
                                     break;
                                 }
                             }
 
-                            if (!match)
+                            if (!match) // if one pixel is different we move to the next tile
                             {
                                 break;
                             }
                         }
 
-                        if (match)
+                        if (match) // if there is a match we create a point and add it to the array of the results 
                         {
                             int matchX = x + bitmapToFind.Width / 2 + offset[k].X;
                             int matchY = y + bitmapToFind.Height / 2 + offset[k].Y;
@@ -228,10 +275,13 @@ namespace ImageSearch
             }
             return results;
         }
-
+        // This is the eucldian search function 
         private static Point?[] FindBitMapEuclidian(Bitmap[] bitmapsToSearch, Bitmap bitmapToFind, Point[] offset)
         {
             Point?[] results = new Point?[bitmapsToSearch.Length];
+            /*
+             *  Here we iterate over each pixel of the tile and compare them to the small image 
+             */
             for (int k = 0; k < bitmapsToSearch.Length; k++)
             {
                 for (int x = 0; x < bitmapsToSearch[k].Width - bitmapToFind.Width + 1; x++)
@@ -244,13 +294,16 @@ namespace ImageSearch
                         {
                             for (int j = 0; j < bitmapToFind.Height; j++)
                             {
+                                /*
+                                 * Here we collect the red green and blue values of the pixels that we are comparing now 
+                                 */
                                 int redBig = bitmapsToSearch[k].GetPixel(x + i, y + j).R;
                                 int redSmall = bitmapToFind.GetPixel(i, j).R;
                                 int greenBig = bitmapsToSearch[k].GetPixel(x + i, y + j).G;
                                 int greenSmall = bitmapToFind.GetPixel(i, j).G;
                                 int blueSmall = bitmapsToSearch[k].GetPixel(x + i, y + j).B;
                                 int blueBig = bitmapToFind.GetPixel(i, j).B;
-
+                                // here we calculate the euclidian distance and check if its equal to zero 
                                 if (Math.Sqrt(((redBig - redSmall) * (redBig - redSmall) +
                                                (greenBig - greenSmall) * (greenBig - greenSmall) +
                                                (blueSmall - blueBig) * (blueSmall - blueBig))) != 0)
@@ -265,7 +318,7 @@ namespace ImageSearch
                                 break;
                             }
                         }
-
+                        // if all the pixels of the tile pass the test then we add them to the result array 
                         if (match)
                         {
                             int matchX = x + bitmapToFind.Width / 2 + offset[k].X;
--- ImageSearch/ImageSearch/MTMergeSort.cs
+++ ImageSearch/ImageSearch/MTMergeSort.cs
@@ -8,11 +8,10 @@ namespace ImageSearch
 {
     public static class MTMergeSort
     {
-        /*public List <string> MergeSort(string[] starList, int nMin = 2)
-        {
-            Sort(starList);
-            return null;
-        }*/
+        /**
+         * This is the main function of the merge sort, this function calls the recursive merge sort.
+         * After the merge sort is done the output is then converted to a list which then returned to the user.*
+         */
         public static List<string> MergeSort(string[] starList, int nMin = 2)
         {
             Sort(starList, nMin);
@@ -20,50 +19,54 @@ namespace ImageSearch
             return result;
         }
 
-        public static void Sort(string[] startList, int nMin)
+        /**
+         *This is the recursive sort function that splits the array into two sub arrays and then merges it*
+         */
+        private static void Sort(string[] startList, int nMin)
         {
-            
-            if (startList.Length > nMin)
+            if (startList.Length > nMin) // this is the the minimal threshold for crating a new tread for the sort, over-wise it just sorts the array manually 
             {
+                //We split the array into two halfs 
                 int mid = startList.Length / 2;
 
                 string[] firstHalf = new string[mid];
                 string[] secondHalf = new string[startList.Length - mid];
                 Array.Copy(startList,0, firstHalf, 0,mid);
                 Array.Copy(startList, mid, secondHalf, 0, startList.Length - mid);
-
+                // then we create two threads that each will run the sort function with half of the original array
                 Thread threadFirstHalf = new Thread(() => Sort(firstHalf,nMin));
                 Thread threadSecondHalf = new Thread(() => Sort(secondHalf,nMin));
-
+                // we launch the threads
                 threadFirstHalf.Start();
                 threadSecondHalf.Start();
-
+                // we wait for the threads to finish
                 threadFirstHalf.Join();
                 threadSecondHalf.Join();
-
+                // then we merge the results 
                 Merge(firstHalf, secondHalf, startList);
             }
-            else
+            
+            else // in case the array size is smaller then the nMin
             {
                 Array.Sort(startList);
             }
-
-            /*if (startList.Length > 1)
-            {
-                string[] arrays = new string[]
-            }*/
         }
-        
+        /**
+         *This is the merge function it receives three arrays - two half arrays that are sorted and then merges it to the third array*
+         */
         private static void Merge(string[] first, string[] second, string[] arr)
         {
             int currFirst = 0;
             int currSecond = 0;
             int currArr = 0;
 
-            while(currFirst < first.Length && currSecond < second.Length)
-            {
-                if (string.Compare(first[currFirst], second[currSecond]) == -1) // second is bigger
-                    {
+            while(currFirst < first.Length && currSecond < second.Length) 
+            { 
+                /*
+                 * This part merges the sub arrays into the big array*
+                 */
+                if (string.Compare(first[currFirst], second[currSecond]) == -1) // compares the strings
+                    { 
                         arr[currArr] = first[currFirst];
                         currFirst++;
                         currArr++;
@@ -74,27 +77,10 @@ namespace ImageSearch
                         currSecond++;
                         currArr++;
                     }
-                
-                
-                
-                    /*if (string.Compare(first[currFirst], second[currSecond]) == 1) // first is bigger
-                    {
-                        arr[currArr] = second[currSecond];
-                        currSecond++;
-                        currArr++;
-                    }
-                    
-                    else
-                    {
-                        arr[currArr] = first[currFirst];
-                        currFirst++;
-                        currArr++;
-                        arr[currArr] = second[currSecond];
-                        currSecond++;
-                        currArr++;
-                    }*/
             }
-
+            /*
+             * fills the remaining elements to the big array*
+             */
             while (currFirst < first.Length)
             {
                 arr[currArr] = first[currFirst];
@@ -109,42 +95,4 @@ namespace ImageSearch
             }
         }
     }
-
-
-
-
-
-    /*class MyTask
-    {
-        private string[] starList;
-        
-        public MyTask(string[] arr)
-        {
-            this.starList = arr;
-        }
-
-        public void Compute()
-        {
-            if (starList.Length > 1)
-            {
-                int mid = starList.Length / 2;
-
-                string[] firstHalf = new string[mid];
-                string[] secondHalf = new string[starList.Length - mid];
-                Array.Copy(starList,firstHalf,mid);
-                Array.Copy(starList, mid, secondHalf, 0, starList.Length - mid);
-
-                Task firstTask = Task.Factory.StartNew(() => new MyTask(firstHalf).Compute());
-                Task secondTask = Task.Factory.StartNew(() => new MyTask(secondHalf).Compute());
-                Task.WaitAll(firstTask, secondTask);
-
-                MTMergeSort.Merge(firstHalf, secondHalf,starList);
-
-            }
-        }
-    }*/
-    
-
-
-
 }
